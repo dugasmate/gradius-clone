83b92c11075a292e7b14f43e5483581d9989505f
dugasmate <dugasmate@gmail.com>
2018-11-07 18:41:50 +0100

    add image

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 let app = new PIXI.Application();
 
 document.body.appendChild(app.view);
+
+PIXI.loader
+  .add("./images/spaceship.png")
+  .load(setup);
+
+function setup() {
+  let spaceship = new PIXI.Sprite(
+    PIXI.loader.resources["./images/spaceship.png"].texture);
+  document.body.appendChild(spaceship);
+};
